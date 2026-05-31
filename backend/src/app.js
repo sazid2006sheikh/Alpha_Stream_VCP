ab9701ed7da8f7cafbbb00a6e52d275f0bc84d26
Sazid2006Sheikh <sazid20063008@gmail.com>
2026-06-01 01:08:21 +0530

    Security-enhanced-dotenv

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,6 +9,9 @@ import { connectToSocket } from "./controllers/socketmanager.js";
 import cors from "cors";
 import userRoutes from "./models/routes/user_routes.js";
 
+import dotenv from "dotenv";
+dotenv.config();
+
 const app = express();
 const server = createServer(app);
 const io = connectToSocket(server);
@@ -20,20 +23,23 @@ app.use(express.json({ limit: "40kb" }));
 app.use(express.urlencoded({ limit: "40kb", extended: true }));
 
 app.use("/api/v1/users", userRoutes);
+app.get("/", (req, res) => {
+    res.send("Alpha-Stream Backend Running Successfully !!");
+});
 
 const start = async () => {
     app.set("mongo_user")
-    const connectiondb = await mongoose.connect("mongodb+srv://sazidapnavideo:110044@cluster0.lvrfvrw.mongodb.net/");
+    const connectiondb = await mongoose.connect(process.env.MONGODB_URL);
 
     console.log(`MONGO Connected DB HOst: ${connectiondb.connection.host}`)
     server.listen(app.get("port"), () => {
-        console.log("LISTENIN ON PORT 8000")
+        console.log(`LISTENING ON PORT ${app.get("port")}`)                 
     });
-
-
 
 }
 
 
-
-start();
+start().catch(err => {
+    console.error("FULL ERROR:");
+    console.error(err);
+});
